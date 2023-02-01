5e68783d868666dc65c49084f00808f214e681cf
Sander <slehtoja@gmail.com>
2023-02-01 15:02:00 +0200

    Nupud on loodud

--- Controller.py
+++ Controller.py
@@ -1,6 +1,7 @@
 from Model import Model
 from View import View
 
+
 class Controller:
 
     def __init__(self):
@@ -9,3 +10,19 @@ class Controller:
 
     def main(self):
         self.view.main()
+
+    def click_btn_name(self):
+        self.view.btn_name['state'] = 'normal'
+
+    def click_btn_job(self):
+        self.view.btn_task['state'] = 'normal'
+
+    def click_btn_shuffle(self):
+        self.view.btn_shuffle['state'] = 'normal'
+
+    def click_btn_save(self):
+        self.view.btn_save['state'] = 'normal'
+
+    def click_btn_clear(self):
+        self.view.btn_clear['state'] = 'normal'
+
--- View.py
+++ View.py
@@ -5,6 +5,7 @@ class View(Tk):
 
     def __init__(self, controller, model):
         super().__init__()
+        self.default_style = None
         self.controller = controller
         self.model = model
 
@@ -15,6 +16,9 @@ class View(Tk):
 
         # Create three frames
         self.frame_top, self.frame_bottom = self.create_two_frames()
+
+        # Create all buttons
+        self.btn_name, self.btn_task, self.btn_shuffle, self.btn_save, self.btn_clear = self.create_all_buttons()
 
     def main(self):
         self.mainloop()
@@ -46,3 +50,28 @@ class View(Tk):
         frame_bottom.pack(expand=True, fill='both')
 
         return frame_top, frame_bottom  # method return two objects
+
+    def create_all_buttons(self):
+        # Create button name
+        btn_name = Button(self.frame_top, text='Name', font=self.default_style,
+                          command=self.controller.click_btn_name)
+        # Create button job
+        btn_task = Button(self.frame_top, text='Task', font=self.default_style,
+                          command=self.controller.click_btn_job)
+        # Create button shuffle
+        btn_shuffle = Button(self.frame_top, text='Shuffle', font=self.default_style,
+                             command=self.controller.click_btn_shuffle)
+        # Create button save
+        btn_save = Button(self.frame_top, text='Save', font=self.default_style,
+                          command=self.controller.click_btn_save)
+        # Create button clear
+        btn_clear = Button(self.frame_top, text='clear', font=self.default_style,
+                          command=self.controller.click_btn_clear)
+
+        btn_name.grid(row=0, column=0, padx=5, pady=2, sticky=EW)
+        btn_task.grid(row=0, column=2, padx=5, pady=2, sticky=EW)
+        btn_shuffle.grid(row=0, column=4, padx=5, pady=2, sticky=EW)
+        btn_save.grid(row=0, column=6, padx=5, pady=2, sticky=EW)
+        btn_clear.grid(row=0, column=8, padx=5, pady=2, sticky=EW)
+
+        return btn_name, btn_task, btn_shuffle, btn_save, btn_clear
