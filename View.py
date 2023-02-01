7162fa58a8af1b319da60a6139702d16abd2dc41
Sander <slehtoja@gmail.com>
2023-02-01 13:14:59 +0200

    Python finale alustatud

---
+++ View.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+class View(Tk):
+
+    def __init__(self, controller, model):
+        super().__init__()
+        self.controller = controller
+        self.model = model
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+class Python_finale:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    Python_finale()
