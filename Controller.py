a6ee566ca6cb9e3f21edefe4f6ecbcad19b2f154
Sander <slehtoja@gmail.com>
2023-02-03 16:30:34 +0200

    TODO Tehtud

--- Controller.py
+++ Controller.py
@@ -19,6 +19,7 @@ class Controller:
         self.view.textbox_names.delete('1.0', 'end')
         names = filedialog.askopenfilename(filetypes=[("txt file", ".txt")])
         if names != '':
+            mes.showerror('Error', 'Valitu fail on tühi')
             self.model.open_file_names(names)
             if len(self.model.names) > 0:
                 for names in self.model.names:
@@ -28,6 +29,7 @@ class Controller:
         self.view.textbox_task.delete('1.0', 'end')
         task = filedialog.askopenfilename(filetypes=[("txt file", ".txt")])
         if task != '':
+            mes.showerror('Error', 'Valitu fail on tühi')
             self.model.open_file_task(task)
             if len(self.model.task) > 0:
                 for task in self.model.task:
@@ -46,18 +48,20 @@ class Controller:
                 x += 1
 
     def click_btn_save(self):
-        if self.model.shuffle != "":
-            mes.showerror('Error', 'List on liiga lühike')
+        if len(self.model.shuffle) != 0:
             shuffle = filedialog.asksaveasfilename(
                 filetypes=[("txt file", ".txt")],
                 defaultextension=".txt",
                 initialdir='D:\\my_data\\my_html\\')
             print(shuffle)
-            if shuffle != "":
+            if shuffle != '':
                 """ File exists"""
                 with open(shuffle, "a", encoding="utf-8") as f:
                     for save in self.model.shuffle:
                         f.write(save + "\n")
 
+
+
         # TODO enne salvestamist kontrollida kas 3. listis on midagi enne kui salvestada saab
+        # TODO create button clear
 
