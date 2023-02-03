bf4f9003e7e0e831a1c396253bdf29166376be15
Sander <slehtoja@gmail.com>
2023-02-03 09:47:46 +0200

    Nupud tehtud

--- Controller.py
+++ Controller.py
@@ -12,16 +12,36 @@ class Controller:
         self.view.main()
 
     def click_btn_name(self):
-        self.view.btn_name['state'] = 'normal'
+        self.view.btn_name['state'] = 'disabled'
+        self.view.btn_task['state'] = 'normal'
+        self.view.btn_shuffle['state'] = 'normal'
+        self.view.btn_save['state'] = 'normal'
+        self.view.btn_clear['state'] = 'normal'
 
     def click_btn_job(self):
-        self.view.btn_task['state'] = 'normal'
+        self.view.btn_name['state'] = 'normal'
+        self.view.btn_task['state'] = 'disabled'
+        self.view.btn_shuffle['state'] = 'normal'
+        self.view.btn_save['state'] = 'normal'
+        self.view.btn_clear['state'] = 'normal'
 
     def click_btn_shuffle(self):
-        self.view.btn_shuffle['state'] = 'normal'
+        self.view.btn_name['state'] = 'normal'
+        self.view.btn_task['state'] = 'normal'
+        self.view.btn_shuffle['state'] = 'disabled'
+        self.view.btn_save['state'] = 'normal'
+        self.view.btn_clear['state'] = 'normal'
 
     def click_btn_save(self):
-        self.view.btn_save['state'] = 'normal'
+        self.view.btn_name['state'] = 'normal'
+        self.view.btn_task['state'] = 'normal'
+        self.view.btn_shuffle['state'] = 'normal'
+        self.view.btn_save['state'] = 'disabled'
+        self.view.btn_clear['state'] = 'normal'
 
     def click_btn_clear(self):
-        self.view.btn_clear['state'] = 'normal'
+        self.view.btn_name['state'] = 'normal'
+        self.view.btn_task['state'] = 'normal'
+        self.view.btn_shuffle['state'] = 'normal'
+        self.view.btn_save['state'] = 'normal'
+        self.view.btn_clear['state'] = 'disabled'
